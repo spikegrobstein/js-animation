6a9514ce52c6ad5ed651d272f023d19cc080ca67
Spike Grobstein <me@spike.cx>
2014-09-05 20:06:29 -0400

    added bouncing guy

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
   gameEnvironment.keyboard.handle( 'A', function(key, state) { console.log('pressed: ' + key) });
   gameEnvironment.keyboard.handle( 'B', function(key, state) { console.log('pressed: ' + key) });
 
-  var e = new Entity({
+  new Entity({
     x: 0,
     y: 50,
     bus: gameEnvironment.bus,
@@ -24,10 +24,42 @@
 
       this.moveTo(
         200 + 150 * ( Math.cos( this.properties.rotation ) ),
-        200 + 150 * ( Math.sin( this.properties.rotation ) )
+        200 + 150 * ( Math.sin( this.properties.rotation ) ),
+        true
       )
     }
   });
+
+  new Entity({
+    x: 50,
+    y: 0,
+    bus: gameEnvironment.bus,
+    class: 'rocket',
+    properties: {
+      rotation: 0,
+      xv: 50, // pixels per second
+      yv: 0,
+      bounces: 0
+    },
+    frameHandler: function( tick, timeDelta, now ) {
+      this.properties.yv += 10;
+
+      if ( this.y > 460 ) {
+        this.properties.yv = -this.properties.yv * .9;
+        this.properties.bounces += 1;
+      }
+
+      if ( this.x > 500 || this.x < 0 ) {
+        this.properties.xv = -this.properties.xv * .9;
+      }
+
+      this.moveDelta(
+        timeDelta / 1000 * this.properties.xv,
+        timeDelta / 1000 * this.properties.yv,
+        true
+      )
+    }
+  })
 
   gameEnvironment.run();
 })(window,document);
